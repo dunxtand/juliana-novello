fe0632cffa220963047facb73c29057e3c050fb3
dunxtand <mdrstandish@gmail.com>
2022-01-09 10:24:08 -0500

    Remove unused template files.

--- wp-content/themes/Tailbase/index.php
+++ wp-content/themes/Tailbase/index.php
@@ -1,7 +1,1 @@
 <?php
-
-$data               = Timber::context();
-$data['posts']      = new Timber\PostQuery();
-$data['pagination'] = Timber::get_pagination($pagination_mid_size);
-
-Timber::render('index.twig', $data);
