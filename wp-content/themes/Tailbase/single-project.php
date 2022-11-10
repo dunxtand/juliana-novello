3ab6ef912d9a916d74a3b2bf5a10ded28bdfac57
Duncan Standish <mdrstandish@gmail.com>
2022-11-10 15:36:32 -0500

    Bring down image size in project galleries; compile assets.

--- wp-content/themes/Tailbase/single-project.php
+++ wp-content/themes/Tailbase/single-project.php
@@ -1,12 +1,14 @@
 <?php
 
+include_once get_theme_file_path( '/functions/projects.php' );
+
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 
 $context['project_gallery'] = array_map(
     function ( $image ) {
-        return $image['url'];
+        return $image['sizes']['1536x1536'] ?: $image['url'];
     },
     $post->meta('gallery') ?: array()
 );
