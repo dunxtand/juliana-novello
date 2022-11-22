72895ddc616a4dadff02f0bd95d1c59873743da3
Duncan Standish <mdrstandish@gmail.com>
2022-11-22 18:20:00 -0500

    Change page.php to accomodate custom post types as homepage.

--- wp-content/themes/Tailbase/page.php
+++ wp-content/themes/Tailbase/page.php
@@ -1,7 +1,27 @@
 <?php
 
-$context         = Timber::get_context();
-$post            = new TimberPost();
-$context['post'] = $post;
+$post = new TimberPost();
 
-Timber::render(['page-' . $post->post_name . '.twig', 'page.twig'], $context);
+
+$file = null;
+
+switch ($post->post_type) {
+    case 'post':
+        $file = 'single.php';
+        break;
+    case 'project':
+        $file = 'single-project.php';
+        break;
+    case 'video':
+        $file = 'single-video.php';
+        break;
+}
+
+if ($file) {
+    include get_theme_file_path( $file );
+} else {
+    $context = Timber::get_context();
+    $context['post'] = $post;
+
+    Timber::render(['page-' . $post->post_name . '.twig', 'page.twig'], $context);
+}
